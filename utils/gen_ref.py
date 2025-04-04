6b7a58ed9d00aa929ef7c324adaed84ed17d25bf
Junfeng Lei <junf137@outlook.com>
2025-04-03 21:15:14 -0400

    [feat] generate unique request ID and area calculation for reference predictions

--- utils/gen_ref.py
+++ utils/gen_ref.py
@@ -3,6 +3,7 @@ from ultralytics import YOLO
 import cv2
 import os
 import time
+import uuid
 
 # Load the model (e.g., model_0)
 model = YOLO("./models/best.pt")
@@ -11,7 +12,9 @@ model_name = "model_0"
 # List of reference image paths
 reference_folder = "data/ref_best/images"
 
-predictions = []
+csv_content = []
+
+timestamp = time.time()  # Use a fixed timestamp
 
 # iterate all images under reference_folder
 for filename in os.listdir(reference_folder):
@@ -21,17 +24,30 @@ for filename in os.listdir(reference_folder):
 
     # get full path
     image_path = os.path.join(reference_folder, filename)
+    ref_uuid = str(uuid.uuid4())  # Generate a unique UUID for each image
 
     img = cv2.imread(image_path)
     results = model.predict(img, conf=0.25, imgsz=640)
-    timestamp = time.time()  # Use a fixed or current timestamp
     for r in results:
         for box in r.boxes:
             conf = float(box.conf[0])
             cls_id = int(box.cls[0])
             label = model.names[cls_id]
-            predictions.append({"timestamp": timestamp, "model_name": model_name, "class": label, "confidence": round(conf, 2)})
+
+            x1, y1, x2, y2 = box.xyxy[0].tolist()
+            area = (x2 - x1) * (y2 - y1)
+
+            csv_content.append(
+                {
+                    "request_id": ref_uuid,
+                    "timestamp": timestamp,
+                    "model_name": model_name,
+                    "class": label,
+                    "confidence": conf,
+                    "area": area,
+                }
+            )
 
 # Save to reference_predictions.csv
-df = pd.DataFrame(predictions)
-df.to_csv("reference_predictions.csv", index=False)
+df = pd.DataFrame(csv_content)
+df.to_csv("ref_pred_0.csv", index=False)
